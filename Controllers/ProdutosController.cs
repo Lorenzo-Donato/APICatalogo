bc433f1ffec27acaacce9cdd687f1ed989736eec
Lorenzo Donato <lorenzodonato75@gmail.com>
2026-03-12 17:05:18 -0300

    feat: synchronous delete method in ProdutosController

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -67,4 +67,19 @@ public class ProdutosController : ControllerBase
 
         return Ok(produto);
     }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        if (produto is null)
+        {
+            return NotFound("Objeto nulo");
+        }
+
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
 }
